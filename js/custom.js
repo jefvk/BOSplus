077bacb804f5be06d9a04257c1c7af93e9325a8c
jef vankerckhoven <jef@jefvankerckhoven.be>
2017-11-06 20:24:40 +0100

    added isotope, favicon, home logo button

--- js/custom.js
+++ js/custom.js
@@ -6,3 +6,21 @@ burger.onclick= function(){
   this.classList.toggle('open');
   menu.classList.toggle('inv');
 }
+
+
+$( document ).ready(function() {
+    var $container = $('.grid');
+    // filter buttons
+    $('#filters button').click(function(){
+		var $this = $(this);
+        // don't proceed if already selected
+        if ( !$this.hasClass('is-checked') ) {
+          $this.parents('#options').find('.is-checked').removeClass('is-checked');
+          $this.addClass('is-checked');
+        }
+      var selector = $this.attr('data-filter');
+      $container.isotope({  itemSelector: '.grid-item', filter: selector });
+      return false;
+    });
+
+});
